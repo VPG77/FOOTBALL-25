523293e8e1e4b1450e52289e8d4be223667570df
VPG77 <vicenteandresoseco@gmail.com>
2025-07-08 15:01:42 +0200

    Crear archivo game.js base

---
+++ game.js
@@ -0,0 +1,18 @@
+// Variables globales
+let currentMode = null;
+
+function startQuickMatch() {
+  alert("Modo Partido rápido aún no implementado.");
+}
+
+function startTournament() {
+  alert("Modo Torneo aún no implementado.");
+}
+
+function startCareer() {
+  alert("Modo Carrera aún no implementado.");
+}
+
+function playerAction(action) {
+  alert(`Acción del jugador: ${action}`);
+}
